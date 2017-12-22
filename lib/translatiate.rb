af365f0ce4ec4258cdc45e9cf33ab498e0a98e1c
Will Hall <willhall88@gmail.com>
2017-12-22 12:36:09 +0000

    Add yaml load and key functionality

--- lib/translatiate.rb
+++ lib/translatiate.rb
@@ -2,11 +2,23 @@ require 'translatiate/version'
 require 'yaml'
 
 class Translatiate
+  attr_accessor :file, :keys
   def initialize
+    @keys = []
   end
 
   def load_file(filename)
     file = File.new(filename, "r")
-    YAML::load(file)
+    @file = YAML::load(file)
+  end
+
+  def get_keys(object)
+    if object.is_a?(Hash)
+      object.each do |key, value|
+        @keys << key
+        get_keys(value)
+      end
+    end
+    @keys
   end
 end
--- spec/translatiate_spec.rb
+++ spec/translatiate_spec.rb
@@ -3,12 +3,19 @@ RSpec.describe Translatiate do
     expect(Translatiate::VERSION).not_to be nil
   end
 
-
+  let(:translatiate) { described_class.new }
+  let(:file) { translatiate.file }
+  before(:each) { translatiate.load_file('./spec/dummy/config/locales/en.yml') }
 
   context '#load_file' do
-    subject { described_class.new.load_file('./spec/dummy/config/locales/en.yml') }
-    it 'loads yaml file data' do
-      is_expected.to eq({"en"=>{"hello"=>"Hello world"}})
+    context 'loads yaml file data' do
+      subject { file['en']['hello']}
+      it { is_expected.to eq('Hello world') }
     end
+  end
+
+  context '#get_keys' do
+    subject { translatiate.get_keys(file) }
+    it { is_expected.to eq(%w(en hello goodbye synonyms hi hey)) }
   end
 end
